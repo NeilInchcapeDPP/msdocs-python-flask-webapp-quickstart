adb82d1bd4b191fbd5dc886345d0beabc9f1e3ed
NeilInchcapeDPP <neil.lorena@inchcape.com>
2023-08-01 14:33:57 +0800

    first

--- app.py
+++ app.py
@@ -1,15 +1,35 @@
 import os
+import pyodbc
 
 from flask import (Flask, redirect, render_template, request,
-                   send_from_directory, url_for)
+                   send_from_directory, url_for, jsonify)
 
 app = Flask(__name__)
 
+server = 'az-aue-au-sql-dev-dpp-001.database.windows.net'
+database = 'epc-subaru'
+username = 'Neil'
+password = '3pFt45KD?&d7XLze'
+
+connection_string = f'DRIVER=ODBC Driver 17 for SQL Server;SERVER={server};DATABASE={database};UID={username};PWD={password}'
+
+def get_db_connection():
+    try:
+        connection = pyodbc.connect(connection_string)
+        return connection
+    except pyodbc.Error as e:
+        print(f"Error connecting to the database: {str(e)}")
+        return None
 
 @app.route('/')
 def index():
    print('Request for index page received')
-   return render_template('index.html')
+   connection = get_db_connection()
+   if connection:
+        return render_template('index.html')
+   else:
+        return jsonify({"error": "Failed to connect to the database."}), 500
+   
 
 @app.route('/favicon.ico')
 def favicon():
@@ -26,6 +46,37 @@ def hello():
    else:
        print('Request for hello page received with no name or blank name -- redirecting')
        return redirect(url_for('index'))
+
+@app.route('/vin', methods=['GET'])
+def find_vin():
+    vin = request.args.get('VIN', '')
+    
+    if not vin:
+        return jsonify({"error": "VIN parameter is missing."}), 400
+    connection = get_db_connection()
+
+    if not connection:
+        return jsonify({"error": "Failed connect to the database."}), 500
+
+    cursor = connection.cursor()
+    query = f"SELECT SHASHUCTLG, MODEL FROM [raw].[epcsub_M_SYARYO_210901] WHERE VIN_AFTER = ?"
+    
+    try:
+        cursor.execute(query, (vin, ))
+        row = cursor.fetchone()
+        if row:
+            shshuctlg, model = row
+            return jsonify({"VIN": vin, "SHASHUCTLG": shshuctlg, "MODEL": model})
+        else:
+            return jsonify({"error": "No Records Found"}), 404
+        
+    except pyodbc.Error as e:
+        print(f"Error executing the query: {str(e)}")
+        return jsonify({"error": "Error executing the query."}), 500
+    finally:
+        cursor.close()
+        connection.close()
+   
 
 
 if __name__ == '__main__':
